5da1ab116ad82437eaf86746e2be4406c9671389
Chloé <qhermier@gmail.com>
2024-12-02 16:11:26 +0100

    Finish TP 1 -> 14.

--- app/src/main/java/com/example/todosmaximethomas/list/Task.kt
+++ app/src/main/java/com/example/todosmaximethomas/list/Task.kt
@@ -1,5 +1,5 @@
 package com.example.todosmaximethomas.list
 
-class Task (val id : String,val title : String,val description : String){
+class Task (val id : String,val title : String,val description : String = "Description"){
 
 }
